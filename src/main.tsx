730c56a39fb42e2233fef771a5378d715cf21942
@ivanmmurciaua <solucionesemecuadrado@gmail.com>
2026-04-30 12:19:25 +0200

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import Landing from './Landing.tsx'
 import App from './App.tsx'
@@ -14,5 +15,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )
